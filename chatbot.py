6c0c0aa9e2bd3b0f13c7072dda00326226bea3a5
Kichi779 <92625816+Kichi779@users.noreply.github.com>
2023-03-08 15:26:30 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -17,7 +17,7 @@ print(Colorate.Vertical(Colors.cyan_to_blue, Center.XCenter("""
                              ███ ▀███▄ ███   ███    ███   ███    ███   ███  
                              ███   ▀█▀ █▀    ████████▀    ███    █▀    █▀   
                              ▀                                             
-                    Edit the code, but please don't remove my name. do not make the sale.
+                          Edit the code, but please don't remove my name. 
                                Discord https://discord.gg/aVk4JUFukk       
                                Github  https://github.com/kichi779          
                        
@@ -56,3 +56,26 @@ while True:
         time.sleep(interval)
     except Exception as e:
         print(f"[{time.strftime('%X')}] Message could not be sent: {e}")
+
+
+# ==========================================
+# Copyright 2023 Kichi779
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==========================================
